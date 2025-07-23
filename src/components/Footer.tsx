d9c2ec2a576d6c67ca7c7ed90198c9cb9fe50713
meharahmadft6 <167222938+meharahmadft6@users.noreply.github.com>
2025-07-23 20:44:51 +0500

    Your commit message here

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -44,21 +44,21 @@ export default function Footer() {
             {/* Social Media Icons */}
             <div className="flex space-x-4 mb-6">
               <a
-                href="https://www.linkedin.com/company/cherry-limousine"
+                href="https://www.yelp.com/biz/cherry-limo-redwood-city" // <-- Replace with your real Yelp URL
                 target="_blank"
                 rel="noopener noreferrer"
-                className="w-10 h-10 bg-yellow-500 rounded-full flex items-center justify-center hover:bg-yellow-400 transition-colors duration-300"
+                className="w-10 h-10 bg-red-600 rounded-full flex items-center justify-center hover:bg-red-500 transition-colors duration-300"
               >
                 <svg
-                  className="w-5 h-5"
+                  className="w-5 h-5 text-white"
                   fill="currentColor"
                   viewBox="0 0 24 24"
                 >
-                  <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z" />
+                  <path d="M10.342 2.63c-.527-.275-1.107.315-.9.84l1.888 4.816c.203.52.954.503 1.125-.025l1.728-5.305a.75.75 0 0 0-1.126-.874L10.342 2.63zM4.403 8.43a.75.75 0 0 0-.708.987l1.37 4.093c.179.533.9.654 1.205.206l2.63-3.942a.75.75 0 0 0-.685-1.141l-3.812-.203zM17.695 3.604c-.275-.526-1.094-.41-1.163.178l-.613 5.292a.75.75 0 0 0 1.268.657l3.003-3.796a.75.75 0 0 0-.495-1.33h.001zM21.375 15.438c-.59-1.167-4.122-1.683-4.985-1.69-.864-.007-1.662.637-1.414 1.354l1.518 4.497c.233.69 1.01.98 1.586.586l3.132-2.144c.51-.35.728-1.03.482-1.604h.001zM11.999 14.25c-.748 0-1.353.605-1.353 1.353v4.5a1.353 1.353 0 0 0 2.707 0v-4.5c0-.748-.605-1.353-1.353-1.353z" />
                 </svg>
               </a>
               <a
-                href="https://www.facebook.com/cherryLimousine"
+                href="https://www.facebook.com/cherrylimousines/photos/?ref=page_internal&_rdr"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="w-10 h-10 bg-yellow-500 rounded-full flex items-center justify-center hover:bg-yellow-400 transition-colors duration-300"
@@ -72,17 +72,17 @@ export default function Footer() {
                 </svg>
               </a>
               <a
-                href="https://www.instagram.com/cherrylimousine"
+                href="https://www.weddingwire.com/biz/cherry-limos/2480b947472e0644.html"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="w-10 h-10 bg-yellow-500 rounded-full flex items-center justify-center hover:bg-yellow-400 transition-colors duration-300"
+                className="w-10 h-10 bg-pink-600 rounded-full flex items-center justify-center hover:bg-pink-500 transition-colors duration-300"
               >
                 <svg
-                  className="w-5 h-5"
+                  className="w-5 h-5 text-white"
                   fill="currentColor"
                   viewBox="0 0 24 24"
                 >
-                  <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zM12 0C8.741 0 8.333.014 7.053.072 2.695.272.273 2.69.073 7.052.014 8.333 0 8.741 0 12c0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98C8.333 23.986 8.741 24 12 24c3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98C15.668.014 15.259 0 12 0zm0 5.838a6.162 6.162 0 100 12.324 6.162 6.162 0 000-12.324zM12 16a4 4 0 110-8 4 4 0 010 8zm6.406-11.845a1.44 1.44 0 100 2.881 1.44 1.44 0 000-2.881z" />
+                  <path d="M20.948 4.524c-1.283-1.398-3.392-1.515-4.852-.257l-.718.633-.736-.61c-1.486-1.23-3.617-1.125-4.912.266-1.353 1.454-1.272 3.732.182 5.185l5.462 5.414a.752.752 0 001.063 0l5.36-5.283c1.453-1.453 1.536-3.742.153-5.348zm-1.031 4.363l-4.816 4.748-4.894-4.85c-.893-.893-.965-2.347-.132-3.255.801-.86 2.036-.928 2.94-.15l1.25 1.036a.75.75 0 00.978-.006l1.227-1.082c.887-.783 2.096-.705 2.876.147.847.923.763 2.38-.429 3.412z" />
                 </svg>
               </a>
             </div>
